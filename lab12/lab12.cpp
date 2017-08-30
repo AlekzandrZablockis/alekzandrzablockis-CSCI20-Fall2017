cf25b44a7d28d4449bcdf13790838fe130523917
Alekzandr Zablockis <azablockis001@student.butte.edu>
2017-08-30 19:53:51 +0000

    Lab1.2 Commit

---
+++ lab12/lab12.cpp
@@ -0,0 +1,15 @@
+// Alekzandr Zablockis
+// 8/28/2017
+// ASCII Art
+ #include <iostream>
+ using namespace std;
+ 
+ int main()
+ {
+     cout << "   ._." << endl;
+     cout << "  _| |_" << endl;
+     cout << " |_   _|" << endl;
+     cout << "  _| |_" << endl;
+     cout << " |  _  | " << endl;
+     cout << " |_| |_|" << endl;
+ }
