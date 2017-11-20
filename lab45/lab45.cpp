76f76cbe6577a609f5ede06958fc94c7b606c197
Alekzandr Zablockis <azablockis001@student.butte.edu>
2017-11-20 05:48:34 +0000

    Lab 4.5 Submission

--- lab45/lab45.cpp
+++ lab45/lab45.cpp
@@ -4,22 +4,22 @@
 // These names are a combonation of two names the user entered.
 
 #include <iostream>
-#include <cstring>
+#include <string>
 using namespace std;
 
 int main()
 {
-    char first_name[10]; // Char array
-    char last_name[20]; // Char array
-    int i = 0;   // For the while loop
+    string first_name = ""; // Place holders
+    string last_name = "";
+    int i = 0;   // Sets our loops to keep going
     int i_2 = 0;
     
     while (i < 100) // Loops until a valid first name is put in
     {
         cout << "Please put in your first name. Max characters 10." << endl;
         cin >> first_name;
-    
-        if (strlen(first_name) > 10) // strlen checks the string length to see so we can determine if it is bigger than 10
+        
+        if (first_name.length() > 10)  // .length() creates the length of the string into a number
         {
             cout << "The first name you have entered is too long. Try Again." << endl;
         }
@@ -33,10 +33,10 @@ int main()
     {
         cout << "Please put in your last name. Max characters 20." << endl;
         cin >> last_name;
-    
-        if (strlen(last_name) > 20) // strlen checks the string length to see so we can determine if it is bigger than 20
+        
+        if (last_name.length() > 20) // .length() creates the length of the string into a number
         {
-            cout << "The last name you have entered is too long. Try again." << endl;
+            cout << "The first name you have entered is too long. Try again." << endl;
         }
         else 
         {
@@ -44,17 +44,16 @@ int main()
         }
     }
     
-    if ((strcmp(first_name,last_name) == 0)) // strcmp checks to see the two strings are the same. If they are the smae it equals the value 0.
+    if (first_name == last_name)
     {
-         cout << "The first and last you have entered match" << endl;
+        cout << "The two names you have entered are the same" << endl;
     }
     
     cout << "Name: " << first_name << " " << last_name << endl;
     cout << "Which of the following usernames would you like?" << endl;
-    cout << first_name[0] << first_name [1] << last_name << endl;           // Final output to the user. Uses specifc chars of the char array to create different names out of the two names put in.
+    cout << first_name[0] << first_name [1] << last_name << endl;       // Outputs final output to user with three possible name choices
     cout << first_name[0] << last_name << endl;
     cout << first_name << last_name << endl;
-    
 }
 
 /*
@@ -63,25 +62,24 @@ Please put in your first name. Max characters 10.
 Alekzandr
 Please put in your last name. Max characters 20.
 Zablockis
-Name: Alekzandr ZablockisWhich of the following usernames would you like?
+Name: Alekzandr Zablockis
+Which of the following usernames would you like?
 AlZablockis
 AZablockis
 AlekzandrZablockis
 
 EXAMPLE 2
 Please put in your first name. Max characters 10.
-April 
+April
 Please put in your last name. Max characters 20.
 Browne
-Name: April BrowneWhich of the following usernames would you like?
+Name: April Browne
+Which of the following usernames would you like?
 ApBrowne
 ABrowne
 AprilBrowne
 
 EXAMPLE 3
-Please put in your first name. Max characters 10.
-AnaElizibith
-The first name you have entered is too long. Try Again.
 Please put in your first name. Max characters 10.
 AnaElizabeth
 The first name you have entered is too long. Try Again.
@@ -95,12 +93,13 @@ Please put in your first name. Max characters 10.
 
 EXAMPLE 4
 Please put in your first name. Max characters 10.
-James
+JAMES
 Please put in your last name. Max characters 20.
-James
-The first and last you have entered match
-Name: James JamesWhich of the following usernames would you like?
-JaJames
-JJames
-JamesJames
+JAMES
+The two names you have entered are the same
+Name: JAMES JAMES
+Which of the following usernames would you like?
+JAJAMES
+JJAMES
+JAMESJAMES
 */
